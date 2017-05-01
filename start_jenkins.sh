c22ef02b6a69a4a8ab3328566f7e28843060aff8
root <root@node-1.nomad.corrarello.net>
2017-05-01 22:20:46 +0100

    New start script for isolated exec

---
+++ start_jenkins.sh
@@ -0,0 +1,14 @@
+!#/bin/bash
+
+curl -o vault.zip https://releases.hashicorp.com/vault/0.7.0/vault_0.7.0_linux_arm.zip ; yes | unzip vault.zip
+GITHUB_PAN=$(./vault read -field=pan secret/github)
+JENKINS_HOME=/local
+JENKINS_ARGS="--httpPort=$HTTP_PORT --ajp13Port=$AJP_PORT"
+JENKINS_ARGS="$JENKINS_ARGS --preferredClassLoader=java.net.URLClassLoader"
+VERSION=${JENKINS_VERSION:-"2.46.2"}
+URL="http://ftp-chi.osuosl.org/pub/jenkins/war-stable/${VERSION}/jenkins.war"
+ARGS=${JAVA_ARGS:-"-Xmx768m -Xms384m"}
+git clone https://${GITHUB_PAN}@github.com/ncorrare/jenkins-config.git ${JENKINS_HOME}
+curl -O /tmp/jenkins.war ${URL}
+
+java $ARGS -jar /tmp/jenkins.war $JENKINS_ARGS
