1fc087e88a903e23da4b02b3fda94dbdf975674e
root <root@node-1.nomad.corrarello.net>
2017-05-01 23:00:33 +0100

    Added autodiscovered VAULT_ADDR, changed curl arg

--- start_jenkins.sh
+++ start_jenkins.sh
@@ -1,5 +1,6 @@
 !#/bin/bash
 
+VAULT_ADDR=https://vault.service.lhr.consul:8200
 curl -o vault.zip https://releases.hashicorp.com/vault/0.7.0/vault_0.7.0_linux_arm.zip ; yes | unzip vault.zip
 GITHUB_PAN=$(./vault read -field=pan secret/github)
 JENKINS_HOME=/local/jenkins
@@ -9,6 +10,6 @@ VERSION=${JENKINS_VERSION:-"2.46.2"}
 URL="http://ftp-chi.osuosl.org/pub/jenkins/war-stable/${VERSION}/jenkins.war"
 ARGS=${JAVA_ARGS:-"-Xmx768m -Xms384m"}
 git clone https://${GITHUB_PAN}@github.com/ncorrare/jenkins-config.git ${JENKINS_HOME}
-curl -O /tmp/jenkins.war ${URL}
+curl -o /tmp/jenkins.war ${URL}
 
 java $ARGS -jar /tmp/jenkins.war $JENKINS_ARGS
